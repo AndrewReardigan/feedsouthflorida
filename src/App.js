5d0e2f69a7af089c4adc1a218889dfb5bbd12771
andrewreardigan <andrewreardigan@icloud.com>
2020-10-24 19:14:32 -0400

    api endpoint set for upc response.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
+import logo from './logo.svg';
 import React from 'react'
 import Signup from './components/Signup'
 import './App.css';
 import BarcodeScanner from './components/BarcodeScanner';
-import CheckTheProduct from './components/CheckTheProduct';
 
 function App() {
   return (

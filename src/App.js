d8d581b804bd90547758d04e56de85b8896a8e33
andrewreardigan <andrewreardigan@icloud.com>
2020-10-24 18:32:11 -0400

    input password field added for login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import React from 'react'
 import Signup from './components/Signup'
 import './App.css';
@@ -8,7 +8,7 @@ import CheckTheProduct from './components/CheckTheProduct';
 function App() {
   return (
     <div className="App">
-      <CheckTheProduct />
+      <Signup />
 
     </div>
   );

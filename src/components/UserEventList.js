d3b9d4bd908dcb682882457dcd0bde5d1117fed7
Alison Reilly <reillyamr@gmail.com>
2020-10-24 14:05:06 -0400

    change to divs and potential variables

--- src/components/UserEventList.js
+++ src/components/UserEventList.js
@@ -3,16 +3,29 @@ import React from 'react';
 function UserEvents(props) {
 
     const nearbyEvents = props.events.map(e => 
-        <li key={e.id}>
-          <Link to={`/user-events/${e.id}`}>{e.event_name}</Link>
-        </li>
+        <div key={e.id}>
+            <Link to={`/user-events/${e.id}`}>
+                
+              <div class="event-goal">{e.pounds_goal}</div> {/* or current pounds */}
+              <div class="event-image">{e.image}</div>
+              <div class="event-title">{e.event_name}</div>
+              
+            </Link>
+        </div>
     
         )
 
     const weeklyEvents = props.events.map(e => 
-        <li key={e.id}>
-          <Link to={`/user-events/${e.id}`}>{e.event_name}</Link>
-        </li>
+        props.events.map(e => 
+            <div key={e.id}>
+                <Link to={`/user-events/${e.id}`}>
+                    
+                  <div class="event-goal">{e.pounds_goal}</div> {/* or current pounds */}
+                  <div class="event-image">{e.image}</div>
+                  <div class="event-title">{e.event_name}</div>
+                  
+                </Link>
+            </div>
         
         )
 

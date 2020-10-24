fd159980c8ec819238c7b775fcbb356156235cc6
slowell <seth.lowell@gmail.com>
2020-10-24 14:14:07 -0400

    Add firebase to package.json

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -1,6 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+
 const config = {
 	apiKey: process.env.REACT_APP_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,

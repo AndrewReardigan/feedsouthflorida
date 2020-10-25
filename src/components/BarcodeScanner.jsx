c787ea4e1809c316147a7259d5a5c96f9478c4c7
andrewreardigan <andrewreardigan@icloud.com>
2020-10-24 23:12:00 -0400

    rendering scan page edits

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,55 +1,47 @@
-import React, { useState } from 'react'
-import BarcodeScannerComponent from "react-webcam-barcode-scanner";
-import CheckTheProduct from './CheckTheProduct'
-import '../App.css'
+import React, { useState } from 'react';
+import BarcodeScannerComponent from 'react-webcam-barcode-scanner';
+import CheckTheProduct from './CheckTheProduct';
+import '../App.css';
 
 const BarcodeScanner = () => {
-    
- 
-        const [ data, setData ] = useState(null);
+	const [ data, setData ] = useState(null);
 
-        const [found, setFound] = useState(false)
+	const [ found, setFound ] = useState(false);
 
-        const itemData = () => {
+	const itemData = () => {};
 
-        }
-
-
-const videoConstraints = {
-
-    aspectRatio: 0.6666666667
-  };
-       
-        return (
-          <>
-          <div className="scan-header">DONATE</div>
-            <BarcodeScannerComponent
-            className="scanner"
-              width="100%"
-            //   height={720}
-              margin="0"
-              padding="0"
-              videoConstraints={videoConstraints}
-                  onUpdate={(err, result) => {
-
-                    if (!found && result){
-                     setData(result.text)
-                     setFound(true)
-                     console.log(found)
-                    }
-                    // else setData('Not Found')
-                  }}
-            />
-            {/* <p>{data}</p>
+	return (
+		<div>
+			<div className="scan-header">DONATE</div>
+			<BarcodeScannerComponent
+				className="scanner"
+				width="100%"
+				height="100%"
+				onUpdate={(err, result) => {
+					if (!found && result) {
+						setData(result.text);
+						setFound(true);
+						console.log(found);
+					}
+					// else setData('Not Found')
+				}}
+			/>
+			{/* <p>{data}</p>
             <p>{console.log(data)}</p> */}
-            <CheckTheProduct barCode={data}/>
+			{/* <CheckTheProduct barCode={data} /> */}
 
+			<div
+				style={{
+					marginTop: '-50px',
+					zIndex: 99999999,
+					width: '80%',
+					height: '200px',
+					backgroundColor: 'white',
+					display: 'inline-block'
+				}}
+			/>
+		</div>
+	);
 };
 
-          </>
-        )
-      }
-
-
-
-export default BarcodeScanner
+export default BarcodeScanner;

0c1e0e993fa8c12515368d75675706d17c0f2926
Alison Reilly <reillyamr@gmail.com>
2020-10-24 21:43:07 -0400

    create  donate container for donation features

---
+++ src/containers/Donate.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+
+
+class Donations extends Component {
+
+  render() {
+       
+        return (
+            <div>
+                {/* cont to hold barcode, picture and text input */}
+                {/* comp for forms */}
+                {/* comp for working donations list/additions */}
+                
+  
+            </div>
+  
+    );
+  }
+
+}
+
+
+export default Donations;
